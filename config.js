0a348ef4f0bf9767fb1b3816cdb92c203811fd61
Tamer6340 <160175134+Tamer6340@users.noreply.github.com>
2024-04-18 21:03:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bot: {
+    owners: ["951925631099207720" , "1076256190503321641"],// owners id
+    botID: "1180251249560522925",// bot id
+    GuildId: "1068938684554018836", // Your Server Id
+    ClientId: "1106230290977914920", // Clietn Id
+    serverinvte: "https://discord.gg/WRbSssdZ", // Server invite url
+    clientSECRET: "Wc4ecIcWV3vEXPO9Au1iX9tqtWDyFGd1", // Client Secret
+    callbackURL: "https://bletoyt-bot-verification.tmraabdltwb.repl.co/login", // Call Back Url
+    inviteBotUrl: "https://discord.com/api/oauth2/authorize?client_id=1103382321450733648&permissions=8&redirect_uri=https%3A%2F%2Fbletoyt-bot-verification.bletoyt.repl.co%2Flogin&scope=bot",
+    TOKEN: (process.env.token) // Bot Token
+  },
+  website: {
+    PORT: "3004",//website port
+  }
+}
